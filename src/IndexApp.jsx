6e7387c2395fe0d021b05d0dc6d6e2dfd1ad4b79
JosueReyes-1 <josueisrae222@gmail.com>
2022-09-14 02:59:50 +0000

    first commit

---
+++ src/IndexApp.jsx
@@ -0,0 +1,16 @@
+import { NavBar } from "./components/NavBar"
+
+
+
+
+
+export const IndexApp = () => {
+
+    
+    return (
+        <>
+            <NavBar/>
+        </>
+
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { IndexApp } from './IndexApp'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <IndexApp/>
+  </React.StrictMode>
+)
