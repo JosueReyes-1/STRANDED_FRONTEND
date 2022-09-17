b77777ba72adc13f4a70a37aca659148ea75b462
JosueReyes-1 <josueisrae222@gmail.com>
2022-09-17 00:44:01 +0000

    added owl-carousel for categories

---
+++ src/components/Category.jsx
@@ -0,0 +1,39 @@
+import { Grid,Button } from "@nextui-org/react";
+import { ButtonCategory } from "../components";
+
+import { faChampagneGlasses, faUmbrellaBeach } from "@fortawesome/free-solid-svg-icons";
+
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.min.css';
+import 'owl.carousel/dist/assets/owl.theme.default.min.css';
+
+import '../css/owlcarousel.css'
+
+export const Category = () => {
+    return (
+        <>
+
+            <Grid.Container gap={2} css={{ jc: "center" }} >
+                <OwlCarousel margin={10} autoWidth className='owl-category'>
+                    <ButtonCategory category={'Playa'} icon={faUmbrellaBeach} />
+                    <ButtonCategory category={'Bar'} icon={faChampagneGlasses} />
+                    <ButtonCategory category={'Playa'} icon={faUmbrellaBeach} />
+                    <ButtonCategory category={'Bar'} icon={faChampagneGlasses} />
+                    <ButtonCategory category={'Playa'} icon={faUmbrellaBeach} />
+                    <ButtonCategory category={'Bar'} icon={faChampagneGlasses} />
+                    <ButtonCategory category={'Playa'} icon={faUmbrellaBeach} />
+                    <ButtonCategory category={'Bar'} icon={faChampagneGlasses} />
+                    <ButtonCategory category={'Playa'} icon={faUmbrellaBeach} />
+                    <ButtonCategory category={'Bar'} icon={faChampagneGlasses} />
+                    <ButtonCategory category={'Playa'} icon={faUmbrellaBeach} />
+                    <ButtonCategory category={'Bar'} icon={faChampagneGlasses} />
+                    <ButtonCategory category={'Playa'} icon={faUmbrellaBeach} />
+                    <ButtonCategory category={'Bar'} icon={faChampagneGlasses} />
+                </OwlCarousel>
+
+            </Grid.Container>
+
+
+        </>
+    )
+}
