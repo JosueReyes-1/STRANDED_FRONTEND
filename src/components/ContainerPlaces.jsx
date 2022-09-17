120e78fd65f8c85fb943da2bbd6fa96d491b52db
JosueReyes-1 <josueisrae222@gmail.com>
2022-09-17 03:57:59 +0000

    added a component for the places list

---
+++ src/components/ContainerPlaces.jsx
@@ -0,0 +1,19 @@
+
+import { CardPlaces } from "./CardPlaces";
+import './css/ContainerPlaces.css'
+export const ContainerPlaces = () => {
+    return (
+        <>
+            <div className="container_places rounded">
+                <CardPlaces />
+                <CardPlaces />
+                <CardPlaces />
+                <CardPlaces />
+                <CardPlaces />
+                <CardPlaces />
+                <CardPlaces />
+            </div>
+
+        </>
+    )
+}
